70cb2b6db493b5458bb00720ca255af641c6e92c
Xuanwo <xuanwo.cn@gmail.com>
2018-05-22 11:39:19 +0800

    source: fs: Support glob filepath
    Signed-off-by: Xuanwo <xuanwo.cn@gmail.com>

--- source/fs/fs.go
+++ source/fs/fs.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 
 	"gopkg.in/yaml.v2"
 
@@ -13,32 +14,47 @@ import (
 
 // Fs will load data from file system.
 type Fs struct {
-	Path string
+	files []string
 }
 
 // Init will initiate Fs.
 func (f *Fs) Init(c config.Endpoint) (err error) {
-	f.Path = c.Path
+	f.files, err = filepath.Glob(c.Path)
+	if err != nil {
+		return
+	}
+	if f.files == nil {
+		log.Printf("No file matched")
+		return
+	}
+
 	return
 }
 
 // List will list all servers from Fs.
 func (f *Fs) List() (s []model.Server, err error) {
-	file, err := os.OpenFile(f.Path, os.O_RDONLY, 0600)
-	if err != nil {
-		log.Printf("Open file failed for %v.", err)
-		return
-	}
-	defer file.Close()
+	s = []model.Server{}
 
-	content, err := ioutil.ReadAll(file)
-	if err != nil {
-		return
-	}
+	for _, v := range f.files {
+		file, err := os.OpenFile(v, os.O_RDONLY, 0600)
+		if err != nil {
+			log.Printf("Open file failed for %v.", err)
+			return nil, err
+		}
+		defer file.Close()
 
-	err = yaml.Unmarshal(content, &s)
-	if err != nil {
-		return
+		content, err := ioutil.ReadAll(file)
+		if err != nil {
+			return nil, err
+		}
+
+		ts := []model.Server{}
+		err = yaml.Unmarshal(content, &ts)
+		if err != nil {
+			return nil, err
+		}
+
+		s = append(s, ts...)
 	}
 	return
 }
