98dc596fa63e494ecc17fc26b4a7977073cf8dfe
Xuanwo <xuanwo.cn@gmail.com>
2018-06-11 15:18:51 +0800

    config: Refactor to support default setting
    Signed-off-by: Xuanwo <xuanwo.cn@gmail.com>

--- config/endpoint.go
+++ config/endpoint.go
@@ -2,7 +2,8 @@ package config
 
 // Endpoint is the config for source or destination.
 type Endpoint struct {
-	Type    string            `yaml:"type"`
-	Path    string            `yaml:"path"`
-	Options map[string]string `yaml:"options"`
+	Type string `yaml:"type"`
+
+	Options map[string]interface{} `yaml:"options"`
+	Default map[string]interface{} `yaml:"default"`
 }
